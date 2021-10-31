0f9782fb4be6d2791b8800c47b8d4145048187ca
Tushar Shrivastava <tushar.shrivastava@convoy.com>
2021-10-31 16:54:15 +0530

    added header and shop page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 import React from 'react';
 import Homepage from './pages/homepage/homepage.component';
-import { Switch, Route, Link } from 'react-router-dom';
-
-const HatsPage = () => <div>Hats Page</div>;
+import { Switch, Route } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header-component/header-component';
 
 const App = () => {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route path="/shop/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
