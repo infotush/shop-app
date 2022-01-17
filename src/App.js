ee0442d15133cde24f156fafbc2f66aff9e850c7
Tushar Shrivastava <tushar.shrivastava@convoy.com>
2022-01-17 18:16:11 +0530

    toggle cart hide and show

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
 import Homepage from './pages/homepage/homepage.component';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header-component/header-component';
 import AuthPage from './pages/auth-page/auth-page-component';
@@ -40,15 +40,25 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={Homepage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={AuthPage} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              this.props.currentUser ? <Redirect to="/" /> : <AuthPage />
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/redux/cart/cart.types.js
@@ -0,0 +1,3 @@
+export const CardActionTypes = {
+  TOGGLE_CART_DROPDOWN: 'TOGGLE_CART_DROPDOWN',
+};
